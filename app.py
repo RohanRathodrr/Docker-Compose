d58b6ab49673c96d260d98f63549404a24a783ed
Young Minds <109673364+ygminds73@users.noreply.github.com>
2022-09-26 22:07:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello Garrett! I have been seen {} times.\n'.format(count)
+    return 'Hello Young Minds! I have been seen You {} times.\n'.format(count)
